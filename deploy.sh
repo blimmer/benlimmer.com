92eff344d692bfe5b23f83f78721e12ddf244d39
Ben Limmer <ben@benlimmer.com>
2020-01-29 15:28:29 -0700

    Add MD5 sum to .js and .css to enable caching (#14)
    * Add Jekyll-minibundle
    
    * Fingerprint CSS to allow caching
    
    * Add md5 sum to require.js
    
    * Set cache-control headers for 1-year on assets
    
    * update regex

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,14 @@
 #! /bin/bash
 set -e
 
+ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
 rm -fr _site
 bundle exec jekyll build
 cd _site || exit
 
 aws-vault exec benlimmer.com_s3deploy -- s3deploy \
+  -config "$ROOT_DIR"/.s3deploy.yml \
   -region us-east-1 \
   -bucket benlimmer.com \
   -distribution-id E2ZJLY90YUBA3S
