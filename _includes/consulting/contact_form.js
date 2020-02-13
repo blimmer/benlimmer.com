098298f90e7ff2cf30059161e0ac5f58f779dbe7
Ben Limmer <ben@benlimmer.com>
2020-02-13 15:10:16 -0700

    A few layout tweaks

--- _includes/consulting/contact_form.js
+++ _includes/consulting/contact_form.js
@@ -1,47 +1,47 @@
 // This is a bit old-school, but the only javascript interaction on the site
 // right now.
 
-window.onload = function () {
-  if (!window.fetch) {
-    var alert = $('.alert-danger');
-    alert.text("You're using a very old browser, so this form won't work. Please email me at hello@benlimmer.com.");
-    alert.show();
-    $('#contact-form').hide();
-    return;
-  }
+if (!window.fetch) {
+  var alert = $('.alert-danger');
+  alert.text("You're using a very old browser, so this form won't work. Please email me at hello@benlimmer.com.");
+  alert.show();
+  $('#contact-form').hide();
+  return;
+}
 
-  $('#contact-form').submit(function (e) {
-    e.preventDefault();
+$('#contact-form').submit(function (e) {
+  e.preventDefault();
 
-    $('#contact-error').hide();
+  $('#contact-error').hide();
 
-    var $form = $(this);
+  var $form = $(this);
 
-    $form.find('button[type="submit"').prop('disabled', true);
+  $form.find('button[type="submit"').prop('disabled', true);
 
-    var name = $form.find("#name-input").val(),
-      email = $form.find("#email-input").val(),
-      message = $form.find('#message-input').val();
+  var name = $form.find("#name-input").val(),
+    email = $form.find("#email-input").val(),
+    message = $form.find('#message-input').val();
 
-    fetch('https://qy9tjykiya.execute-api.us-east-1.amazonaws.com/production/contact', {
-      method: 'POST',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        name: name,
-        email: email,
-        message: message
-      })
-    }).then(function () {
-      $('#contact-success').show();
-      $form.hide();
-    }).catch(function (e) {
-      console.error(e);
-      var alert = $('#contact-error');
-      alert.show();
-      $form.find('button[type="submit"').prop('disabled', false);
+  fetch('https://qy9tjykiya.execute-api.us-east-1.amazonaws.com/production/contact', {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      name: name,
+      email: email,
+      message: message
+    })
+  }).then(function () {
+    $('#contact-success').show();
+    $form.find('.form-control').each(function () {
+      $(this).prop('disabled', true);
     });
+  }).catch(function (e) {
+    console.error(e);
+    var alert = $('#contact-error');
+    alert.show();
+    $form.find('button[type="submit"').prop('disabled', false);
   });
-};
+});
