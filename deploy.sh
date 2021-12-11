5536fc0bb91662cf8d49a10712dde662d8a6dbac
Ben Limmer <ben@benlimmer.com>
2021-12-11 14:14:47 -0700

    feat: create consulting redirects via deploy

--- deploy.sh
+++ deploy.sh
@@ -2,21 +2,30 @@
 set -e
 
 ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+BUCKET=benlimmer.com
 
 rm -fr _site
 bundle exec jekyll build
 cd _site || exit
 
-if [[ -n $CI ]]; then
-  "$GOPATH"/bin/s3deploy \
-      -config "$ROOT_DIR"/.s3deploy.yml \
-      -region us-east-1 \
-      -bucket benlimmer.com \
-      -distribution-id E2S061LCTHO4BI
-else
-  aws-vault exec benlimmer.com_s3deploy -- s3deploy \
+# Sync Site
+"$GOPATH"/bin/s3deploy \
     -config "$ROOT_DIR"/.s3deploy.yml \
     -region us-east-1 \
-    -bucket benlimmer.com \
+    -bucket "$BUCKET" \
     -distribution-id E2S061LCTHO4BI
-fi
+
+# Create redirects
+create_redirect() {
+  local old_path="$1"
+  local redirect_location="$2"
+
+  aws s3api put-object \
+    --bucket $BUCKET \
+    --acl public-read \
+    --key "$old_path" \
+    --website-redirect-location "$redirect_location"
+}
+
+create_redirect "consulting/index.html" "/freelance"
+create_redirect "consulting/contact/index.html" "/freelance/contact"
