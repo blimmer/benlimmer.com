062ac2c4ec98858e5caa9bd80d286566921c3800
Ben Limmer <ben@benlimmer.com>
2025-04-25 17:52:59 -0600

    feat: initial site

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,5 +1,6 @@
 export default {
-  plugins: ["prettier-plugin-astro", "prettier-plugin-organize-imports"],
+  plugins: ["prettier-plugin-astro", "prettier-plugin-organize-imports", "prettier-plugin-tailwindcss"],
   printWidth: 120,
   proseWrap: "always",
+  tailwindStylesheet: "./src/styles/global.css",
 };
