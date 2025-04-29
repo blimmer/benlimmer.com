ca7d7ef1ce4ff803010530ff60cf642a8984a688
Ben Limmer <ben@benlimmer.com>
2025-04-29 17:00:51 -0600

    fix: use static generator

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   site: "https://benlimmer.com",
+  output: "static",
   vite: {
     plugins: [tailwindcss()],
   },
