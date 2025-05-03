ae747f137f20c92d29a131fa6f55a64a3e79289b
Ben Limmer <630449+blimmer@users.noreply.github.com>
2025-05-03 16:13:02 -0600

    feat: migrate portfolio (#70)

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,16 @@
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://benlimmer.com",
   output: "static",
+
   vite: {
     plugins: [tailwindcss()],
   },
+
+  integrations: [mdx()],
 });
