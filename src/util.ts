eca54bbfac13a4bfeb51bbae0217bd2ffc68be9e
Ben Limmer <ben@benlimmer.com>
2025-05-04 14:43:15 -0600

    fix: two-digit month/date

--- src/util.ts
+++ src/util.ts
@@ -5,7 +5,12 @@ export function parseBlogPost(post: CollectionEntry<"blog">) {
   const [year, month, day, ...slugParts] = post.id.split("-");
   const dasherizedPostTitle = slugParts.join("-");
   const date = new Temporal.PlainDate(parseInt(year, 10), parseInt(month, 10), parseInt(day, 10));
-  const slug = [date.year, date.month, date.day, dasherizedPostTitle].join("/");
+  const slug = [
+    date.year,
+    String(date.month).padStart(2, "0"),
+    String(date.day).padStart(2, "0"),
+    dasherizedPostTitle,
+  ].join("/");
 
   return {
     ...post,
