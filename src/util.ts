19ddaf4a6efb1314f10c35cdc97c04bbfc73fea4
Ben Limmer <ben@benlimmer.com>
2025-05-04 14:01:32 -0600

    add pagination

--- src/util.ts
+++ src/util.ts
@@ -3,7 +3,16 @@ import { Temporal } from "temporal-polyfill";
 
 export function parseBlogPost(post: CollectionEntry<"blog">) {
   const [year, month, day, ...slugParts] = post.id.split("-");
-  const slug = slugParts.join("-");
+  const dasherizedPostTitle = slugParts.join("-");
   const date = new Temporal.PlainDate(parseInt(year, 10), parseInt(month, 10), parseInt(day, 10));
-  return { date, slug, year, month, day };
+  const slug = [date.year, date.month, date.day, dasherizedPostTitle].join("/");
+
+  return {
+    ...post,
+    slug,
+    date,
+    year,
+    month,
+    day,
+  };
 }
