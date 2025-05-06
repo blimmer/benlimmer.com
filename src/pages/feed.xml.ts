f534314a2aeeffec71c901599b8dc3038961ac39
Ben Limmer <ben@benlimmer.com>
2025-05-06 15:17:40 -0600

    feat: add RSS feed

---
+++ src/pages/feed.xml.ts
@@ -0,0 +1,22 @@
+import rss from "@astrojs/rss";
+import type { AstroUserConfig } from "astro";
+import { getCollection } from "astro:content";
+import { parseBlogPost } from "src/util";
+
+export async function GET(context: AstroUserConfig) {
+  const posts = (await getCollection("blog")).map(parseBlogPost);
+
+  return rss({
+    title: "BenLimmer.com",
+    description: "Musings on developer experience, productivity, and technology trends.",
+    site: context.site!,
+    items: posts.map((post) => ({
+      title: post.data.title,
+      pubDate: new Date(post.date.year, post.date.month - 1, post.date.day),
+      description: post.data.description,
+      categories: post.data.tags,
+      link: `/blog/${post.slug}`,
+    })),
+    customData: `<language>en-us</language>`,
+  });
+}
