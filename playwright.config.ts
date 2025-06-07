1145cd25bafef3a153d152f7a5c2e99c8fe5542a
Ben Limmer <ben@benlimmer.com>
2025-06-07 10:30:52 -0600

    chore: more performance

--- playwright.config.ts
+++ playwright.config.ts
@@ -19,10 +19,10 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  /* Use multiple workers on CI for better performance */
+  workers: process.env.CI ? 4 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: "html",
+  reporter: process.env.CI ? [["github"], ["html"]] : "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     baseURL: "http://localhost:4321",
