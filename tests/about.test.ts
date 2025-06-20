99a0f09cfa2c30833d7db417069ab6ff60be01ea
Ben Limmer <ben@benlimmer.com>
2025-06-20 08:35:04 -0600

    start writing tests

--- tests/about.test.ts
+++
@@ -1,7 +0,0 @@
-import { expect, test } from "@playwright/test";
-
-test("about page loads", async ({ page }) => {
-  await page.goto("/about");
-  await expect(page).toHaveTitle("About | Ben Limmer");
-  await expect(page.getByRole("heading", { name: "About Me" })).toBeVisible();
-});
